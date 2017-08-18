a0beb9ef5d3a4a0918d149b4ca40508cbd46fc36
joshua dodd <joshuadodd@joshuas-MacBook-Pro.local>
2017-08-18 08:45:31 -0400

    readme updates

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
 var express = require("express");
 var bodyParser = require("body-parser");
 
